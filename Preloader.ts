d792b4cedd5920767ce7ee475b72bf4afa80d987
Dmytro Soldatov <cyberdexx@gmail.com>
2025-08-19 13:05:52 +0300

    refactor preloader

--- Preloader.ts
+++ Preloader.ts
@@ -1,14 +1,21 @@
-import { Assets, type AssetsManifest } from 'pixi.js';
+import { ArrayOr, Assets, type AssetsManifest } from 'pixi.js';
 
 /**
  * Preloader class to load assets for the game
  */
 export class Preloader {
+  private initiated = false;
+
   /**
    * Module to load assets
    * @param basePath - The base path for the assets
    */
-  constructor(private basePath?: string) { }
+  constructor(private manifest: AssetsManifest, private basePath?: string) { }
+
+  private async init() {
+    await Assets.init({ manifest: this.manifest, basePath: this.basePath });
+    this.initiated = true;
+  }
 
   /**
    * Load assets
@@ -16,8 +23,11 @@ export class Preloader {
    * @param bundles - The bundles to be loaded
    * @param onProgress - A callback function to be called on progress
    */
-  async load(manifest: AssetsManifest, bundles: string[] = [], onProgress?: (progress: number) => void): Promise<void> {
-    await Assets.init({ manifest: manifest, basePath: this.basePath });
+  async load(bundles: ArrayOr<string>, onProgress?: (progress: number) => void): Promise<void> {
+    if (!this.initiated) {
+      await this.init();
+    }
+
     await Assets.loadBundle(bundles, (progress) => onProgress?.(progress * 100));
 
     onProgress?.(100);
